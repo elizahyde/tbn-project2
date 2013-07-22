be2a31ab3a9e99b7e7e532c3de356bf7dc232a08
elizahyde <elizabeth@elizahyde.com>
2013-07-22 09:20:26 -0700

    updating all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nurtureus::Application.config.secret_token = 'b0905d3501d12871c2807125b80be3a703eb9ccb5937a5d78ee51895a17a39646d518fbf9ce3ef11828bf943a3b2b87f49bbb77561aee9b389d82b3697431de6'
